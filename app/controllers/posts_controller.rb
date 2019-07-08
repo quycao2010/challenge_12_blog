58ec31a6f31956e9bf1dc98c7a4346396f0d3688
quycao2010 <quycao2010@gmail.com>
2019-07-08 14:52:25 +0700

    Add login for page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @posts = Post.all.order('created_at DESC')
@@ -21,7 +22,7 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to root_path, notice: 'Post was created successfully'
     else
-      render :back
+      redirect_to root_path, notice: 'Post was created not successfully'
     end 
   end
   
