c7e7c2ffbacdea1c50adda6a112ebf33445d7518
quycao2010 <quycao2010@gmail.com>
2019-07-08 10:07:01 +0700

    Add comment for post

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all      
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+  private
+
+  def comment_params
+    params[:comment].permit(:name, :body)
+  end
+end
